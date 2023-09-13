b64671d58eed8394cc5e0766bef14b194407eb1e
Dan Shin <dans@nakahara-2022.local>
2023-09-12 17:54:52 -0700

    Now fetches data from weather api and added child component to render weather data.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted, watch } from 'vue'
+import ChildComponent from './components/ChildComponent.vue';
+
+const data = ref('')
+const currentWeatherData = ref('')
+const message = ref('Hello')
+const API_URL = 'https://api.open-meteo.com/v1/forecast?latitude=49.058&longitude=-122.2526&hourly=temperature_2m,relativehumidity_2m&current_weather=true'
+
+watch(data, (newData) =>{
+  if (newData != '') {
+    currentWeatherData.value = newData.current_weather
+  }
+})
+
+function fetchData(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => data.value = json)
+    .catch((error) => console.log(`Error: ${error}`))
+}
+
+onMounted(() => {
+  fetchData(API_URL)
+})
 
 </script>
 
 <template>
-
+  <h1>Parent</h1>
+  <p v-if="data == ''">LOADING</p>
+  <ChildComponent :current-weather-data="currentWeatherData" :message="message"/>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+    currentWeatherData: Object,
+    message: String
+})
+
+</script>
+
+<template>
+    <p>Data: {{ currentWeatherData }}</p>
+    <p>Message: {{ message }}</p>
+</template>
+
+<style scoped></style>
